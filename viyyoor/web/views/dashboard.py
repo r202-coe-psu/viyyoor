ddc676e0327208edc072cdd6e92c44ab6b6b58e9
Thanathip Limna <boatkrap@gmail.com>
2022-01-24 13:27:41 +0700

    fix dashboard order

--- viyyoor/web/views/dashboard.py
+++ viyyoor/web/views/dashboard.py
@@ -19,18 +19,35 @@ def index_admin():
     # }
     # print("q", queries)
     classes_set = set()
+    endorsed_query = dict()
+    endorses_query = dict()
+
     for ep in endorser_positions:
         queries = {f"endorsers__{ep[0]}__user": current_user._get_current_object()}
+        endorsed_query[
+            f"endorsements__{ep[0]}__endorser"
+        ] = current_user._get_current_object()
+        endorses_query[
+            f"endorsements__{ep[0]}__endorser__ne"
+        ] = current_user._get_current_object()
         sub_classes = models.Class.objects(**queries)
         classes_set.update(sub_classes)
 
     classes = list(classes_set)
+
     endorses_classes = models.Certificate.objects(
-        status="prerelease", class___in=classes
+        status="prerelease", class___in=classes, **endorses_query
     ).distinct(field="class_")
 
+    query_obj = None
+    for k, v in endorsed_query.items():
+        if query_obj:
+            query_obj = query_obj | me.Q(**{k: v})
+        else:
+            query_obj = me.Q(**{k: v})
+
     endorsed_classes = models.Certificate.objects(
-        status="completed", class___in=classes
+        query_obj, class___in=classes
     ).distinct(field="class_")
 
     endorses_classes.sort(key=lambda c: c.id, reverse=True)
