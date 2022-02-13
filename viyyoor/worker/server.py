f4ae9018c8af422027c143a6837ed15120503db0
Thanathip Limna <boatkrap@gmail.com>
2022-02-14 00:14:38 +0700

    change redis config

--- viyyoor/worker/server.py
+++ viyyoor/worker/server.py
@@ -17,10 +17,6 @@ logger = logging.getLogger(__name__)
 
 listen = ["default"]
 
-redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
-
-conn = redis.from_url(redis_url)
-
 
 class ViyyoorWorker(SimpleWorker):
     def __init__(self, *args, **kwargs):
@@ -33,6 +29,9 @@ class ViyyoorWorker(SimpleWorker):
 class WorkerServer:
     def __init__(self, settings):
         self.settings = settings
+
+        redis_url = settings.get("REDIS_URL", "redis://localhost:6379")
+        self.conn = redis.from_url(redis_url)
 
     def run(self):
         with Connection(conn):
